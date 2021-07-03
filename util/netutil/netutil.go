53e8dcda7bf3807ae78bfddee330a9c8b4c08733
whoisnian <zhuchangbao1998@gmail.com>
2021-07-04 01:12:35 +0800

    ADD: netutil.GetOutBoundIP()

---
+++ util/netutil/netutil.go
@@ -0,0 +1,17 @@
+// Package netutil implements some network utility functions.
+package netutil
+
+import "net"
+
+// GetOutBoundIP get preferred outbound ip of current process.
+// https://stackoverflow.com/a/37382208
+func GetOutBoundIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP, nil
+}
