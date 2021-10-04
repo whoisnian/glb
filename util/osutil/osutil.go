7c1b5a16d4a8f164d37ec9f36db8844aa2267b03
whoisnian <zhuchangbao1998@gmail.com>
2021-10-04 17:37:41 +0800

    ADD: osutil.WaitForInterrupt()

---
+++ util/osutil/osutil.go
@@ -0,0 +1,14 @@
+// Package osutil implements some OS utility functions.
+package osutil
+
+import (
+	"os"
+	"os/signal"
+)
+
+// WaitForInterrupt blocks until current process receives SIGINT.
+func WaitForInterrupt() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
