8b37ea3b8d63f26c846a89dec5e6881115bcd0e7
whoisnian <zhuchangbao1998@gmail.com>
2022-06-14 00:42:22 +0800

    ADD: strutil

---
+++ util/strutil/strutil_test.go
@@ -0,0 +1,81 @@
+package strutil
+
+import (
+	"testing"
+)
+
+func TestSliceContain(t *testing.T) {
+	var tests = []struct {
+		inputSlice []string
+		inputValue string
+		want       bool
+	}{
+		{nil, "", false},
+		{[]string{}, "", false},
+		{[]string{""}, "", true},
+		{[]string{"a"}, "a", true},
+		{[]string{"a", "b", "c"}, "c", true},
+		{[]string{"a", "b", "c"}, "d", false},
+	}
+	for _, test := range tests {
+		if got := SliceContain(test.inputSlice, test.inputValue); got != test.want {
+			t.Errorf("SliceContain(%q, %q) = %v, want %v", test.inputSlice, test.inputValue, got, test.want)
+		}
+	}
+}
+
+func TestShellEscape(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  string
+	}{
+		{``, `''`},
+		{`/tmp/dir 1`, `'/tmp/dir 1'`},
+		{`"/tmp/dir 1"`, `'"/tmp/dir 1"'`},
+		{`'/tmp/dir 1'`, `''"'"'/tmp/dir 1'"'"''`},
+		{`~/doc`, `'~/doc'`},
+	}
+	for _, test := range tests {
+		if got := ShellEscape(test.input); got != test.want {
+			t.Errorf("ShellEscape(%q) = %q, want %q", test.input, got, test.want)
+		}
+	}
+}
+
+func TestShellEscapeExceptTilde(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  string
+	}{
+		{``, `''`},
+		{`/tmp/dir 1`, `'/tmp/dir 1'`},
+		{`"/tmp/dir 1"`, `'"/tmp/dir 1"'`},
+		{`'/tmp/dir 1'`, `''"'"'/tmp/dir 1'"'"''`},
+		{`~/doc`, `~/'doc'`},
+	}
+	for _, test := range tests {
+		if got := ShellEscapeExceptTilde(test.input); got != test.want {
+			t.Errorf("ShellEscapeExceptTilde(%q) = %q, want %q", test.input, got, test.want)
+		}
+	}
+}
+
+func TestIsDigitString(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", false},
+		{"-1", false},
+		{"0.1", false},
+		{"1e5", false},
+		{"0", true},
+		{"001", true},
+		{"12345", true},
+	}
+	for _, test := range tests {
+		if got := IsDigitString(test.input); got != test.want {
+			t.Errorf("IsDigitString(%q) = %v, want %v", test.input, got, test.want)
+		}
+	}
+}
