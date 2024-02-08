e8f2c9be27d8bfdbee3d4f3935681a8a439c0b57
whoisnian <zhuchangbao1998@gmail.com>
2024-02-08 22:04:28 +0800

    ADD: ioutil.ReadRand()

--- util/ioutil/ioutil.go
+++ util/ioutil/ioutil.go
@@ -3,6 +3,7 @@ package ioutil
 
 import (
 	"io"
+	"math/rand/v2"
 	"os"
 )
 
@@ -13,4 +14,20 @@ func SeekAndReadAll(fi *os.File) ([]byte, error) {
 		return []byte{}, err
 	}
 	return io.ReadAll(fi)
+}
+
+// ReadRand read len(buf) bytes from *rand.Rand and writes them into buf.
+// It always returns len(buf) and a nil error.
+func ReadRand(r *rand.Rand, buf []byte) (n int, err error) {
+	var pos, val uint64
+	for n = 0; n < len(buf); n++ {
+		if pos == 0 {
+			val = r.Uint64()
+			pos = 8
+		}
+		buf[n] = byte(val)
+		val >>= 8
+		pos--
+	}
+	return n, nil
 }
