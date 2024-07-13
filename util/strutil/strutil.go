b4539288681ff4ef4625837759117e5fc17e882c
whoisnian <zhuchangbao1998@gmail.com>
2024-07-14 03:57:19 +0800

    ADD: strutil: ASCII Camelize() & Underscore()

--- util/strutil/strutil.go
+++ util/strutil/strutil.go
@@ -29,14 +29,90 @@ func ShellEscapeExceptTilde(s string) string {
 	return ShellEscape(s)
 }
 
-// IsDigitString checks if a string only contains digits.
+// IsDigitString checks if a string only contains ASCII digits.
 func IsDigitString(s string) bool {
-	for _, c := range s {
-		if c < '0' || c > '9' {
+	for i := 0; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
 			return false
 		}
 	}
 	return s != ""
+}
+
+// Camelize converts an ASCII string to camel case like string, and supports specifying the case of the first letter.
+func Camelize(s string, upper bool) string {
+	var buf []byte
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		switch {
+		case 'a' <= c && c <= 'z':
+			if upper {
+				c -= 'a' - 'A' // lowercase to uppercase
+				upper = false
+			}
+			buf = append(buf, c)
+		case 'A' <= c && c <= 'Z':
+			if !upper {
+				c += 'a' - 'A' // uppercase to lowercase
+			}
+			upper = false
+			buf = append(buf, c)
+		case '0' <= c && c <= '9':
+			buf = append(buf, c)
+		default:
+			upper = len(buf) > 0 || upper // keep `upper` for first letter in buf
+		}
+	}
+	return string(buf)
+}
+
+// Underscore converts an ASCII string to snake case like string, and supports specifying the case of all letters.
+func Underscore(s string, upper bool) string {
+	const (
+		initial     = iota
+		upperLetter // [A-Z]
+		lowerLetter // [a-z]
+		notAlphanum // [^A-Za-z0-9]
+	)
+	var last int
+	var buf []byte
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		switch {
+		case 'a' <= c && c <= 'z':
+			if upper {
+				c -= 'a' - 'A' // lowercase to uppercase
+			}
+			if len(buf) > 0 && last == notAlphanum {
+				buf = append(buf, '_', c)
+			} else {
+				buf = append(buf, c)
+			}
+			last = lowerLetter
+		case 'A' <= c && c <= 'Z':
+			if !upper {
+				c += 'a' - 'A' // uppercase to lowercase
+			}
+			if len(buf) > 0 && (last == lowerLetter || last == notAlphanum) {
+				buf = append(buf, '_', c)
+			} else if len(buf) > 0 && i+1 < len(s) && 'a' <= s[i+1] && s[i+1] <= 'z' { // next letter is lowercase
+				buf = append(buf, '_', c) // ABc => A_Bc
+			} else {
+				buf = append(buf, c)
+			}
+			last = upperLetter
+		case '0' <= c && c <= '9':
+			if len(buf) > 0 && last == notAlphanum {
+				buf = append(buf, '_', c)
+			} else {
+				buf = append(buf, c)
+			}
+			last = initial
+		default:
+			last = notAlphanum
+		}
+	}
+	return string(buf)
 }
 
 // UnsafeStringToBytes converts string to byte slice without allocation.
--- util/strutil/strutil_test.go
+++ util/strutil/strutil_test.go
@@ -83,6 +83,80 @@ func TestIsDigitString(t *testing.T) {
 	}
 }
 
+func TestCamelize(t *testing.T) {
+	var tests = []struct {
+		input string
+		upper bool
+		want  string
+	}{
+		{"", true, ""},
+		{",.?|", true, ""},
+		{"user_id", true, "UserId"},
+		{"user_id", false, "userId"},
+		{"user.name", true, "UserName"},
+		{"UserName", true, "Username"},
+		{"UserName", false, "username"},
+		{"_csrf_token", true, "CsrfToken"},
+		{"_csrf_token", false, "csrfToken"},
+		{"::Net::HTTP", true, "NetHttp"},
+		{"::Net::HTTP", false, "netHttp"},
+		{"accept-encoding", true, "AcceptEncoding"},
+		{"SHA256 hash", true, "Sha256Hash"},
+		{"s3 access key", true, "S3AccessKey"},
+		{"9P protocol", true, "9PProtocol"},
+		{"Regular 4G", true, "Regular4G"},
+		{"Mysql DSN", true, "MysqlDsn"},
+		{"hex2bin", true, "Hex2bin"},
+		{"/var/log/nginx/", true, "VarLogNginx"},
+		{"SCREAMING_SNAKE_CASE", true, "ScreamingSnakeCase"},
+		{"Test with + sign", true, "TestWithSign"},
+		{"!@#$bad %^&* characters()-=", true, "BadCharacters"},
+	}
+	for _, test := range tests {
+		if got := strutil.Camelize(test.input, test.upper); got != test.want {
+			t.Errorf("Camelize(%q, %t) = %q, want %q", test.input, test.upper, got, test.want)
+		}
+	}
+}
+
+func TestUnderscore(t *testing.T) {
+	var tests = []struct {
+		input string
+		upper bool
+		want  string
+	}{
+		{"", false, ""},
+		{",.?|", false, ""},
+		{"userId", false, "user_id"},
+		{"UserID", false, "user_id"},
+		{"UserID", true, "USER_ID"},
+		{"user.name", false, "user_name"},
+		{"user_name", false, "user_name"},
+		{"UserName", false, "user_name"},
+		{"UserName", true, "USER_NAME"},
+		{"_csrf_token", false, "csrf_token"},
+		{"_csrf_token", true, "CSRF_TOKEN"},
+		{"::Net::HTTP", false, "net_http"},
+		{"::Net::HTTP", true, "NET_HTTP"},
+		{"Accept-Encoding", false, "accept_encoding"},
+		{"SHA256 hash", false, "sha256_hash"},
+		{"S3AccessKey", false, "s3_access_key"},
+		{"9PProtocol", false, "9p_protocol"},
+		{"Regular 4G", false, "regular_4g"},
+		{"MysqlDSN", false, "mysql_dsn"},
+		{"Hex2bin", false, "hex2bin"},
+		{"/var/log/nginx/", false, "var_log_nginx"},
+		{"SCREAMING_SNAKE_CASE", false, "screaming_snake_case"},
+		{"Test with + sign", false, "test_with_sign"},
+		{"!@#$bad %^&* characters()-=", false, "bad_characters"},
+	}
+	for _, test := range tests {
+		if got := strutil.Underscore(test.input, test.upper); got != test.want {
+			t.Errorf("Underscore(%q, %t) = %q, want %q", test.input, test.upper, got, test.want)
+		}
+	}
+}
+
 func TestUnsafeStringToBytes(t *testing.T) {
 	var inputs = []string{
 		"",
