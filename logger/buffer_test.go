a14b9083b973d03e04feb5aba1d38e2113d695b8
whoisnian <zhuchangbao1998@gmail.com>
2024-07-15 02:16:35 +0800

    CHG: logger: test BufferPool for data race

--- logger/buffer_test.go
+++ logger/buffer_test.go
@@ -1,42 +1,35 @@
 package logger
 
 import (
-	"crypto/md5"
 	"fmt"
-	"math/rand/v2"
 	"testing"
-	"time"
-
-	"github.com/whoisnian/glb/util/ioutil"
 )
 
-func TestBufferPool(t *testing.T) {
-	var SEED = uint64(time.Now().Unix())
-	t.Logf("Running tests with PCG seed (0,%v)", SEED)
-	rd := rand.New(rand.NewPCG(0, SEED))
-
-	var (
-		tests   = 1 << 10
-		size    int
-		lastSum [16]byte
-		lastCap = initBufferSize
-	)
-	for i := 0; i < tests; i++ {
-		buf := newBuffer()
-		// Any item stored in the Pool may be removed automatically at any time without notification.
-		if cap(*buf) > initBufferSize {
-			if cap(*buf) != lastCap {
-				t.Fatalf("cap(newBuffer) = %d, want %d", cap(*buf), lastCap)
+func TestBufferPoolRace(t *testing.T) {
+	const P = 10
+	const N = 10000
+	done := make(chan struct{})
+	for i := 0; i < P; i++ {
+		go func() {
+			defer func() { done <- struct{}{} }()
+			for j := 0; j < N; j++ {
+				buf := newBuffer()
+				*buf = append(*buf, make([]byte, j)...) // len(*buf) should be j
+				for k := 0; k < j; k++ {
+					(*buf)[k] = byte(j % 256)
+				}
+				for k := 0; k < len(*buf); k++ {
+					if (*buf)[k] != byte(j%256) {
+						t.Errorf("goroutine(%d) read buf[%d] = %d, want %d", i, k, (*buf)[k], j%256)
+						return
+					}
+				}
+				freeBuffer(buf)
 			}
-			if lastSum != md5.Sum((*buf)[:size]) {
-				t.Fatalf("md5(newBuffer) = %q, want %q", md5.Sum((*buf)[:size]), lastSum)
-			}
-		}
-		*buf = append(*buf, make([]byte, rd.IntN(maxBufferSize*1.5))...)
-		size, _ = ioutil.ReadRand(rd, *buf)
-		lastSum = md5.Sum((*buf)[:size])
-		lastCap = cap(*buf)
-		freeBuffer(buf)
+		}()
+	}
+	for i := 0; i < P; i++ {
+		<-done
 	}
 }
 
